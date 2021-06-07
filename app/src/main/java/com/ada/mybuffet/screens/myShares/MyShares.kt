a43914a4511715332b96e5f177f1a13341762fa5
MarioEberth <marioeberth@gmail.com>
2021-06-07 13:09:11 +0200

    update

--- app/src/main/java/com/ada/mybuffet/screens/myShares/MyShares.kt
+++ app/src/main/java/com/ada/mybuffet/screens/myShares/MyShares.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.ada.mybuffet.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,7 +19,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MyShares.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ShareDetail : Fragment() {
+class MyShares : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +37,17 @@ class ShareDetail : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share_detail, container, false)
+        return inflater.inflate(R.layout.fragment_myshares, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var btn = view.findViewById<Button>(R.id.myShares_bT_detail)
+
+        btn.setOnClickListener {
+            findNavController().navigate(R.id.action_myShares_to_shareDetail2)
+        }
     }
 
     companion object {
