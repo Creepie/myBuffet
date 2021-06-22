bf9a88be64bf456109fc23a829ce70662a1f0a41
Harald Pinsker <p-h@gmx.at>
2021-06-22 16:40:05 +0200

    switch to view binding

--- app/src/main/java/com/ada/mybuffet/screens/login/Login.kt
+++ app/src/main/java/com/ada/mybuffet/screens/login/Login.kt
@@ -3,30 +3,32 @@ package com.ada.mybuffet.screens.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import com.ada.mybuffet.R
+import com.ada.mybuffet.databinding.ActivityLoginBinding
 import com.ada.mybuffet.screens.home.Home
-import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        login_bT_registerMail.setOnClickListener {
+        binding.loginBTRegisterMail.setOnClickListener {
             // go back to calling activity
             finish()
         }
 
-        login_bT_logIn.setOnClickListener {
+        binding.loginBTLogIn.setOnClickListener {
             performLogin()
         }
     }
 
     private fun performLogin() {
-        val email = login_et_email.text.toString()
-        val password = login_et_password.text.toString()
+        val email = binding.loginEtEmail.text.toString()
+        val password = binding.loginEtPassword.text.toString()
 
         val i = Intent(this, Home::class.java)
         startActivity(i)
--- app/src/main/java/com/ada/mybuffet/screens/login/Register.kt
+++ app/src/main/java/com/ada/mybuffet/screens/login/Register.kt
@@ -3,21 +3,29 @@ package com.ada.mybuffet.screens.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.ada.mybuffet.R
+import com.ada.mybuffet.databinding.ActivityLoginBinding
+import com.ada.mybuffet.databinding.ActivityRegisterBinding
 import com.ada.mybuffet.screens.home.Home
-import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_register.*
+import com.google.firebase.auth.FirebaseAuth
 
 class Register : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        register_bT_logIn.setOnClickListener {
+        binding.registerBTLogIn.setOnClickListener {
             performRegistration()
         }
 
-        register_tv_go_to_login.setOnClickListener {
+        binding.registerTvGoToLogin.setOnClickListener {
             val i = Intent(this, Login::class.java)
             startActivity(i)
         }
@@ -25,12 +33,34 @@ class Register : AppCompatActivity() {
 
     private fun performRegistration() {
         // get values of text fields
-        val email = register_et_email.text.toString()
-        val password = register_et_password.text.toString()
-        val passwordRepeated = register_et_repeat_password.text.toString()
+        val email = binding.registerEtEmail.text.toString()
+        val password = binding.registerEtPassword.text.toString()
+        val passwordRepeated = binding.registerEtRepeatPassword.text.toString()
+
+        if (email.isEmpty() || password.isEmpty() || passwordRepeated.isEmpty()) {
+            Toast.makeText(applicationContext, applicationContext.getString(R.string.register_fields_not_filled), Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (password != passwordRepeated) {
+            Toast.makeText(applicationContext, applicationContext.getString(R.string.register_passwords_dont_match), Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (password.length < 6 || password.length > 40) {
+            Toast.makeText(applicationContext, applicationContext.getString(R.string.register_password_invalid), Toast.LENGTH_SHORT).show()
+            return
+        }
 
         // create user in firebase
-        Firebase
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) {
+                    return@addOnCompleteListener
+                }
+
+                Log.d("register.kt", "successful log in, uid: ${it.result?.user?.uid}")
+            }
 
 
         val i = Intent(this, Home::class.java)
