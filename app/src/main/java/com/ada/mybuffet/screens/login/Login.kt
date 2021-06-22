0d781b958734ed9bd0ef1db91cc3b9e11932fbf2
Harald Pinsker <p-h@gmx.at>
2021-06-22 14:58:25 +0200

    UI design for login screen

--- app/src/main/java/com/ada/mybuffet/screens/login/Login.kt
+++ app/src/main/java/com/ada/mybuffet/screens/login/Login.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.ada.mybuffet.R
 import com.ada.mybuffet.screens.home.Home
 
@@ -14,6 +15,12 @@ class Login : AppCompatActivity() {
 
         var register = findViewById<Button>(R.id.login_bT_registerMail)
         var login = findViewById<Button>(R.id.login_bT_logIn)
+
+        // live data callback:
+        // var textfield = findViewById<TextView>(R.id.login_bT_logIn)
+        // if (blabla) {
+        //    textfield.setTextColor()
+        //}
 
         register.setOnClickListener {
            var i = Intent(this,Register::class.java)
