a1f7ef160d942b25a0cb8dace9d06b7b0eee1688
Harald Pinsker <p-h@gmx.at>
2021-06-24 23:36:37 +0200

    added all icons...changed bottom nav colors

--- app/src/main/java/com/ada/mybuffet/screens/myShares/MyShares.kt
+++ app/src/main/java/com/ada/mybuffet/screens/myShares/MyShares.kt
@@ -1,5 +1,7 @@
 package com.ada.mybuffet.screens.myShares
 
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RectShape
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,6 +11,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ada.mybuffet.R
 import com.ada.mybuffet.databinding.FragmentMysharesBinding
@@ -61,6 +64,15 @@ class MyShares : Fragment() {
         val stocksRecyclerView = binding.mySharesRecyclerViewStocks
         stocksRecyclerView.adapter = SharesListAdapter(arrayListOf<ShareItem>())    // init empty
         stocksRecyclerView.layoutManager = LinearLayoutManager(activity)
+
+        // add vertical inter-item spacing of 8dp
+        stocksRecyclerView.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL).also { deco ->
+            with(ShapeDrawable(RectShape())) {
+                intrinsicHeight = (resources.displayMetrics.density * 8).toInt()
+                alpha = 0
+                deco.setDrawable(this)
+            }
+        })
 
         viewModel.shareItems.observe(viewLifecycleOwner, Observer { shareItems ->
             val adapter = SharesListAdapter(shareItems)
