4dc3b1b48f49427ae805eb528d76273ba1bfe449
Harald Pinsker <p-h@gmx.at>
2021-06-24 11:19:18 +0200

    add sign out

--- app/src/main/java/com/ada/mybuffet/screens/profile/Profile.kt
+++ app/src/main/java/com/ada/mybuffet/screens/profile/Profile.kt
@@ -1,11 +1,15 @@
 package com.ada.mybuffet.screens.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ada.mybuffet.R
+import com.ada.mybuffet.databinding.FragmentProfileBinding
+import com.ada.mybuffet.screens.login.Login
+import com.ada.mybuffet.screens.login.Register
+import com.google.firebase.auth.FirebaseAuth
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,25 +22,17 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Profile : Fragment() {
+
+    // set the _binding variable initially to null and
+    // also when the view is destroyed again it has to be set to null
+    private var _binding: FragmentProfileBinding? = null
+
+    // extract the non null value of the _binding with kotlin backing
+    private val binding get() = _binding!!  // only valid between onCreateView & onDestroyView
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
 
     companion object {
         /**
@@ -57,4 +53,40 @@ class Profile : Fragment() {
                 }
             }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.profileBtnLogout.setOnClickListener {
+            // sign out user
+            FirebaseAuth.getInstance().signOut()
+
+            // switch to register activity
+            val i = Intent(activity, Login::class.java)
+            startActivity(i)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+
 }
