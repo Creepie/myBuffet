85f42b0e42fcb9dd2ba7b2621d8793d977935f43
Harald Pinsker <p-h@gmx.at>
2021-06-24 10:44:52 +0200

    keep user signed in & bypass registration screen

--- app/src/main/java/com/ada/mybuffet/MainActivity.kt
+++ app/src/main/java/com/ada/mybuffet/MainActivity.kt
@@ -6,19 +6,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.ada.mybuffet.screens.home.Home
 import com.ada.mybuffet.screens.login.Login
 import com.ada.mybuffet.screens.login.Register
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
 
-        Handler(Looper.getMainLooper()).postDelayed({
+    override fun onStart() {
+        super.onStart()
+
+        val user = Firebase.auth.currentUser
+
+        if (user != null) {
+            // user is signed in --> show home activity
+            val i = Intent(this, Home::class.java)
+            startActivity(i)
+            finish()
+        } else {
+            // user is not signed in --> show login/sign in screen
             val i = Intent(this, Register::class.java)
             startActivity(i)
             finish()
-        }, 3000)
+        }
     }
 }
