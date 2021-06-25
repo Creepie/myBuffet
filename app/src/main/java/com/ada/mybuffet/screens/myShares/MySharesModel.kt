f8257d767402c8965a4ac81aef160a654199b871
Harald Pinsker <p-h@gmx.at>
2021-06-25 13:23:14 +0200

    add all db items and changing up down arrows

--- app/src/main/java/com/ada/mybuffet/screens/myShares/MySharesModel.kt
+++
@@ -1,8 +0,0 @@
-package com.ada.mybuffet.screens.myShares
-
-data class MySharesModel (val authuid: String = "", val email: String = "") {
-
-    override fun toString(): String {
-        return "$authuid $email"
-    }
-}
