2c4ddcd2c72915a5c5c9d0b7569f2611a5554fd8
Harald Pinsker <p-h@gmx.at>
2021-06-27 18:57:02 +0200

    add chart

--- app/src/main/java/com/ada/mybuffet/screens/myShares/MyShares.kt
+++ app/src/main/java/com/ada/mybuffet/screens/myShares/MyShares.kt
@@ -1,30 +1,40 @@
 package com.ada.mybuffet.screens.myShares
 
+import android.graphics.Color
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.RectShape
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.ada.mybuffet.R
 import com.ada.mybuffet.databinding.FragmentMysharesBinding
+import com.ada.mybuffet.screens.myShares.model.PortfolioValueByDate
 import com.ada.mybuffet.screens.myShares.model.ShareItem
-import com.ada.mybuffet.screens.myShares.repo.MySharesRepository
 import com.ada.mybuffet.screens.myShares.repo.MySharesDataProvider
+import com.ada.mybuffet.screens.myShares.repo.MySharesRepository
 import com.ada.mybuffet.screens.myShares.viewModel.MySharesViewModel
 import com.ada.mybuffet.screens.myShares.viewModel.MySharesViewModelFactory
 import com.ada.mybuffet.utils.NumberFormatUtils
 import com.ada.mybuffet.utils.Resource
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.Timestamp
 import java.math.BigDecimal
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -56,6 +66,8 @@ class MyShares : Fragment() {
         )
     }
 
+    private lateinit var portfolioTotalChart: LineChart
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -74,65 +86,121 @@ class MyShares : Fragment() {
     ): View? {
         _binding = FragmentMysharesBinding.inflate(inflater, container, false)
 
-        // recycler view setup
-        val stocksRecyclerView = binding.mySharesRecyclerViewStocks
-        stocksRecyclerView.adapter = SharesListAdapter(arrayListOf<ShareItem>())    // init empty
-        stocksRecyclerView.layoutManager = LinearLayoutManager(activity)
+        setupPortfolioTotalOverview()
+        setupProfitLossOverview()
+        setupRecyclerView()
 
-        // add vertical inter-item spacing of 8dp to recyclerview items
-        stocksRecyclerView.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL).also { deco ->
-            with(ShapeDrawable(RectShape())) {
-                intrinsicHeight = (resources.displayMetrics.density * 8).toInt()
-                alpha = 0
-                deco.setDrawable(this)
-            }
-        })
+        return binding.root
+    }
 
-        viewModel.fetchShareItemList.observe(viewLifecycleOwner, Observer { observedResource ->
+    private fun setupPortfolioTotalOverview() {
+        portfolioTotalChart = binding.mySharesChartTotalPortfolioValue
 
-            when (observedResource) {
-                is Resource.Loading -> {
-                    // could use this to indicate data loading in the UI
-                    // for this you need to use emit(Resource.Loading()) before the try catch
-                    // in the view model
-                    // showProgress()
-                }
+        // remove description text
+        portfolioTotalChart.description.isEnabled = false
 
-                is Resource.Success -> {
-                    // hideProgress()
+        // enable touch gestures
+        portfolioTotalChart.setTouchEnabled(true)
 
-                    val sharesItemsList: MutableList<ShareItem> = observedResource.data as MutableList<ShareItem>
+        // disable scaling and dragging
+        portfolioTotalChart.isDragEnabled = false
+        portfolioTotalChart.setScaleEnabled(false)
+        portfolioTotalChart.setPinchZoom(false)
 
-                    if (sharesItemsList.size == 0) {
-                        // show empty recycler view placeholder message
-                        binding.mySharesTvRecyclerViewEmptyMessage.visibility = View.VISIBLE
+        // remove grid
+        portfolioTotalChart.setDrawGridBackground(false)
 
-                        // hide recycler view
-                        stocksRecyclerView.visibility = View.GONE
-                    } else {
-                        // display recycler view
-                        stocksRecyclerView.visibility = View.VISIBLE
+        // remove axis descriptions and legend
+        portfolioTotalChart.xAxis.isEnabled = false
+        portfolioTotalChart.axisLeft.isEnabled = false
+        portfolioTotalChart.axisRight.isEnabled = false
+        portfolioTotalChart.legend.isEnabled = false
 
-                        // hide placeholder message
-                        binding.mySharesTvRecyclerViewEmptyMessage.visibility = View.GONE
+        // create a custom MarkerView (extend MarkerView) and specify the layout
+        // to use for it
+        // create a custom MarkerView (extend MarkerView) and specify the layout
+        // to use for it
+        val mv = MyMarkerView(this, R.layout.custom_marker_view)
+        mv.setChartView(chart) // For bounds control
 
-                        // connect recyclerview to viewModel data
-                        val adapter = SharesListAdapter(sharesItemsList)
-                        stocksRecyclerView.adapter = adapter
-                    }
+        chart.setMarker(mv) // Set the marker to the chart
 
 
-                }
-
-                is Resource.Failure -> {
-                    // hideProgress()
-                    Toast.makeText(context, "Data could not be loaded", Toast.LENGTH_SHORT).show()
-                }
-            }
-        })
+        // add chart data
+        setPortfolioTotalChartData()
 
 
+        // set animation with duration
+        portfolioTotalChart.animateX(1000)
 
+        // refresh the drawing
+        portfolioTotalChart.invalidate()
+    }
+
+
+    private fun setPortfolioTotalChartData() {
+        val dbItems = ArrayList<PortfolioValueByDate>()
+        dbItems.add(PortfolioValueByDate(Timestamp.now(), "600.00"))
+        dbItems.add(PortfolioValueByDate(Timestamp(Date(1624658400000)), "500.00"))
+        dbItems.add(PortfolioValueByDate(Timestamp(Date(1624572000000)), "550.00"))
+        dbItems.add(PortfolioValueByDate(Timestamp(Date(1624485600000)), "580.00"))
+        dbItems.add(PortfolioValueByDate(Timestamp(Date(1624399200000)), "540.00"))
+
+        val values = ArrayList<Entry>()
+        values.add(Entry(0f, dbItems[0].portfolioTotalValue.toFloat()))
+        values.add(Entry(1f, 450.25f))
+        values.add(Entry(2f, 500.13f))
+        values.add(Entry(3f, 370.77f))
+        values.add(Entry(4f, 390.77f))
+
+
+        var lineDataSet: LineDataSet
+
+        if (portfolioTotalChart.data != null &&
+            portfolioTotalChart.data.dataSetCount > 0
+        ) {
+            lineDataSet = portfolioTotalChart.data.getDataSetByIndex(0) as LineDataSet
+            lineDataSet.values = values
+            lineDataSet.notifyDataSetChanged()
+            portfolioTotalChart.data.notifyDataChanged()
+            portfolioTotalChart.notifyDataSetChanged()
+        } else {
+            // create a dataset and give it a type
+            lineDataSet = LineDataSet(values, "LineDataSet")
+            lineDataSet.setDrawIcons(false)
+
+            // set line mode to cubic to round the edges
+            lineDataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+            lineDataSet.cubicIntensity = 0.2f
+
+            // black lines and points
+            lineDataSet.color = Color.WHITE
+            lineDataSet.setCircleColor(Color.WHITE)
+
+            // line thickness and point size
+            lineDataSet.lineWidth = 1f
+            lineDataSet.circleRadius = 3f
+
+            // draw points as solid circles
+            lineDataSet.setDrawCircleHole(false)
+
+            // text size of values
+            lineDataSet.valueTextColor = Color.WHITE
+            lineDataSet.valueTextSize = 9f
+
+            val dataSets: ArrayList<ILineDataSet> = ArrayList()
+            dataSets.add(lineDataSet) // add the data sets
+
+            // create a data object with the data sets
+            val data = LineData(dataSets)
+
+            // set data
+            portfolioTotalChart.data = data
+        }
+    }
+
+
+    private fun setupProfitLossOverview() {
         viewModel.fetchProfitLossOverviewData.observe(viewLifecycleOwner, Observer { observedResource ->
             when (observedResource) {
                 is Resource.Loading -> {
@@ -203,8 +271,64 @@ class MyShares : Fragment() {
                 }
             }
         })
+    }
 
-        return binding.root
+    private fun setupRecyclerView() {
+        // recycler view setup
+        val stocksRecyclerView = binding.mySharesRecyclerViewStocks
+        stocksRecyclerView.adapter = SharesListAdapter(arrayListOf<ShareItem>())    // init empty
+        stocksRecyclerView.layoutManager = LinearLayoutManager(activity)
+
+        // add vertical inter-item spacing of 8dp to recyclerview items
+        stocksRecyclerView.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL).also { deco ->
+            with(ShapeDrawable(RectShape())) {
+                intrinsicHeight = (resources.displayMetrics.density * 8).toInt()
+                alpha = 0
+                deco.setDrawable(this)
+            }
+        })
+
+        viewModel.fetchShareItemList.observe(viewLifecycleOwner, Observer { observedResource ->
+            when (observedResource) {
+                is Resource.Loading -> {
+                    // could use this to indicate data loading in the UI
+                    // for this you need to use emit(Resource.Loading()) before the try catch
+                    // in the view model
+                    // showProgress()
+                }
+
+                is Resource.Success -> {
+                    // hideProgress()
+
+                    val sharesItemsList: MutableList<ShareItem> = observedResource.data as MutableList<ShareItem>
+
+                    if (sharesItemsList.size == 0) {
+                        // show empty recycler view placeholder message
+                        binding.mySharesTvRecyclerViewEmptyMessage.visibility = View.VISIBLE
+
+                        // hide recycler view
+                        stocksRecyclerView.visibility = View.GONE
+                    } else {
+                        // display recycler view
+                        stocksRecyclerView.visibility = View.VISIBLE
+
+                        // hide placeholder message
+                        binding.mySharesTvRecyclerViewEmptyMessage.visibility = View.GONE
+
+                        // connect recyclerview to viewModel data
+                        val adapter = SharesListAdapter(sharesItemsList)
+                        stocksRecyclerView.adapter = adapter
+                    }
+
+
+                }
+
+                is Resource.Failure -> {
+                    // hideProgress()
+                    Toast.makeText(context, "Data could not be loaded", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
