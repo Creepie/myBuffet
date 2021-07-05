982f2a5f6d6e7c897efb42324d854c42050f4d7d
Harald Pinsker <p-h@gmx.at>
2021-07-05 23:29:54 +0200

    experiment with view model test

--- app/src/test/java/com/ada/mybuffet/screens/myShares/viewModel/MySharesViewModelTest.kt
+++ app/src/test/java/com/ada/mybuffet/screens/myShares/viewModel/MySharesViewModelTest.kt
@@ -3,6 +3,7 @@ package com.ada.mybuffet.screens.myShares.viewModel
 import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asFlow
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ada.mybuffet.FakeMySharesRepository
@@ -12,7 +13,9 @@ import com.ada.mybuffet.screens.myShares.repo.MySharesRepository
 import com.ada.mybuffet.utils.NumberFormatUtils
 import com.ada.mybuffet.utils.Resource
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -23,31 +26,101 @@ import java.util.*
 @RunWith(AndroidJUnit4::class)
 class MySharesViewModelTest {
 
-    @Test
-    fun deleteMeLater() {
-        assertEquals(2, 2)
-    }
-
-    /*
     private lateinit var viewModel: MySharesViewModel
 
     @Before
     fun setUp() {
 
+    }
+
+
+    @Test
+    fun testFetchShareItemList() = runBlockingTest {
+        val repo = FakeMySharesRepository()
+
+        repo.shareItemList.add(
+            ShareItem(
+                "fakeID4",
+                "FKE4",
+                "fakeStockName4",
+                "10.00",
+                "3.72",
+                "10.00",
+                "100.00",
+                10,
+                "5.00",
+                "90.00")
+        )
+
         // "There's no need to use the delegate property or a ViewModelProvider,
         // you can just directly construct the ViewModel in unit tests."
         // according to: https://developer.android.com/codelabs/advanced-android-kotlin-training-testing-test-doubles#6
         viewModel = MySharesViewModelFactory(
-                        MySharesDataProvider(
-                            FakeMySharesRepository()    // inject fake repository
-                        )
-                    ).create(MySharesViewModel::class.java)
-    }
+            MySharesDataProvider(
+                repo   // inject fake repository
+            )
+        ).create(MySharesViewModel::class.java)
 
-    @Test
-    fun testFetchShareItemList() {
-        println("hello")
+        var shareItemList = mutableListOf<ShareItem>()
 
+
+        val observer = Observer<Resource<Any>> {}
+        try {
+            // Observe the LiveData forever
+            viewModel.fetchShareItemList.observeForever(observer)
+
+
+            val value = viewModel.fetchShareItemList.value
+            //assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
+            println("hello2")
+            println(value)
+        } finally {
+            // Whatever happens, don't forget to remove the observer!
+            viewModel.fetchShareItemList.removeObserver(observer)
+        }
+
+
+        /*
+        val job = launch {
+            viewModel.fetchShareItemList.observe()
+
+            viewModel.fetchShareItemList.asFlow().collect() {
+                when (it) {
+                    is Resource.Success -> {
+                        shareItemList.clear()
+                        shareItemList.addAll(it.data as MutableList<ShareItem>)
+                    }
+                }
+            }
+        }
+
+        repo.shareItemList.add(
+            ShareItem(
+                "fakeID5",
+                "FKE5",
+                "fakeStockName5",
+                "10.00",
+                "3.72",
+                "10.00",
+                "100.00",
+                10,
+                "5.00",
+                "90.00")
+        )
+
+
+        shareItemList.forEach {
+            println(it.stockSymbol)
+        }
+
+        assertEquals("FKE1", shareItemList.first().stockSymbol)
+
+        job.cancel()
+
+         */
+
+
+        /*
         // Create observer - no need for it to do anything!
         val observer = Observer<Resource<Any>> {}
         try {
@@ -64,10 +137,15 @@ class MySharesViewModelTest {
             viewModel.fetchShareItemList.removeObserver(observer)
         }
 
+         */
+
     }
 
+
+
+
     @Test
-    fun getFetchProfitLossOverviewData() = runBlocking {
+    fun testFetchShareItemList2() = runBlockingTest {
         val repo = FakeMySharesRepository()
 
         repo.shareItemList.add(
@@ -84,14 +162,17 @@ class MySharesViewModelTest {
             "90.00")
         )
 
-        repo.getShareItemsFromDB().collect {
-            when (it) {
-                is Resource.Success -> {
-                    val data = it.data
-                    assertEquals("FKE1", data.first().stockSymbol)
+        var shareItemList = mutableListOf<ShareItem>()
+
+        val job = launch {
+            repo.getShareItemsFromDB().collect {
+                when (it) {
+                    is Resource.Success -> {
+                        shareItemList.clear()
+                        shareItemList.addAll(it.data)
+                    }
                 }
             }
-
         }
 
         repo.shareItemList.add(
@@ -107,11 +188,21 @@ class MySharesViewModelTest {
                 "5.00",
                 "90.00")
         )
+
+
+        shareItemList.forEach {
+            println(it.stockSymbol)
+        }
+
+        assertEquals("FKE1", shareItemList.first().stockSymbol)
+        assertEquals("FKE5", shareItemList.last().stockSymbol)
+
+
+        job.cancel()
     }
 
     @Test
     fun getFetchTotalPortfolioValueHistory() {
     }
 
-     */
 }
